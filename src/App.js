950d5850902d88b402a596f7c12ef4a2327787ef
AlexandraJaap <119429067+AlexandraJaap@users.noreply.github.com>
2023-04-24 17:54:37 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  let weatherData = {
+    city: "Salzburg",
+    temperature: 8,
+    date: "Friday 21:44 pm",
+    description: "light rain",
+    imgUrl:
+      "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png",
+    humidity: 91,
+    wind: 3,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="weather-wrapper">
+        <div className="weather-app">
+          <form className="mb-3">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="🔎 Enter Location..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-primary w-100"
+                />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>Last updated: {weatherData.date}</li>
+              <li>{weatherData.description}</li>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="d-flex weather-temperature">
+                  <strong>{weatherData.temperature}</strong>
+                  <span className="units">°C</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
